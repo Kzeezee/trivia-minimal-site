91e8b4179db48866e6e78bc8bacd7440429193d1
Kazor Vinci <kazorvinci@gmail.com>
2025-03-06 23:44:13 +0800

    Cloudfare Pages Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-cloudflare';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined
+			}
+		})
 	}
 };
-
-export default config;
